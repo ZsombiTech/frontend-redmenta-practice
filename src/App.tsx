648dcccf0cfa897cd6c71277b4a8a5ef2c14b5a5
ZsombiTech <horvathzsombor2005@gmail.com>
2022-03-22 01:49:53 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import './styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
 import Navbar from "./components/Navbar";
 import MainTitle from "./components/MainTitle";
 import Question from "./components/Question";
@@ -10,36 +10,50 @@ function App() {
   const [datas, setDatas] = useState<Array<string>>();
   const [token, setToken] = useState<string>();
   const [submitted, setSubmitted] = useState<boolean>(false);
-  const [gansw, setGansw] =  useState<number>(0);
+  const [gansw, setGansw] = useState<number>(0);
 
   useEffect(() => {
-    axios.get("http://frozen-hamlet-99845.herokuapp.com/token")
-      .then(res => {
-        const data = res.data;
-        setToken(data[0]);
-      })
-    axios.get("http://frozen-hamlet-99845.herokuapp.com/tasks")
-      .then(res => {
-        const data : Array<string> = res.data;
-        setDatas(data);
-      })
-    
-  }, [])
+    axios.get("http://frozen-hamlet-99845.herokuapp.com/token").then((res) => {
+      const data = res.data;
+      setToken(data[0]);
+    });
+
+    axios.get("http://frozen-hamlet-99845.herokuapp.com/tasks").then((res) => {
+      const data: Array<string> = res.data;
+      setDatas(data);
+    });
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
 
-      <MainTitle submitted={submitted} length={datas && datas.length} gansw={gansw} />
-      
-      {!datas && <h1 className="mt-10 text-xl text-center text-gray-900 sm:text-3xl sm:truncate">Loading data....</h1>}
-      
-      {datas && datas.map((data: string, key: number) => 
-        <Question submitted={submitted} data={data} key={key} token={token} setGansw={setGansw} gansw={gansw}/>
-      )}
-      {datas &&  !submitted && <Submitbutton setSubmitted={setSubmitted} submitted={submitted} />}
-     
+      <MainTitle
+        submitted={submitted}
+        length={datas && datas.length}
+        gansw={gansw}
+      />
 
+      {!datas && (
+        <h1 className="mt-10 text-xl text-center text-gray-900 sm:text-3xl sm:truncate">
+          Loading data....
+        </h1>
+      )}
+
+      {datas &&
+        datas.map((data: string, key: number) => (
+          <Question
+            submitted={submitted}
+            data={data}
+            key={key}
+            token={token}
+            setGansw={setGansw}
+            gansw={gansw}
+          />
+        ))}
+      {datas && !submitted && (
+        <Submitbutton setSubmitted={setSubmitted} submitted={submitted} />
+      )}
     </div>
   );
 }
